1718f5afaf77a7e12afa16feae1be2a72a848bf9
RITESH HARGE <135429163+Ritesh-Harge1@users.noreply.github.com>
2025-09-16 08:17:40 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-// Rendering App component inside root div
-ReactDOM.render(<App />, document.getElementById("root"));
-
-
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
 
 
